ff25e43cf1147e80e946a93bc9ef6df810347340
Ma'rufjon <m.mamatkulov95@gmail.com>
2023-06-23 22:25:41 +0500

    first commit

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+<Navbar/>
+
+
+<router-view/>
+
+</template>
+
+<script>
+import Home from './views/HomeView.vue'
+import Navbar from './components/Navbar.vue'
+
+export default {
+  name:'App',
+  components:{
+    Home,
+    Navbar
+  
+  }
+  
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  max-width: 600px;
+  margin: 0 auto;
+  color: #2c3e50;
+
+}
+body{
+  background-color: cadetblue;
+}
+
+
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,31 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+import AddProject from '../views/AddProject.vue'
+import EditProject from '../views/EditProject.vue'
+
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: HomeView
+  },
+  {
+    path:'/add',
+    name:"addProject",
+    component:AddProject
+  },
+  {
+    path:'/edit/:id',
+    name:"editproject",
+    component:EditProject,
+    props:true 
+  }
+ 
+]
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes
+})
+
+export default router
